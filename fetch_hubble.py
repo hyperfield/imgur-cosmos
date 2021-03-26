d4251cdacaba02c5c5e557c6fb994364617e44aa
sequence <sequence@myrealm.ca>
2021-03-26 09:48:41 -0600

    Brought HTTP error exception handling to outer functions.

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -12,40 +12,34 @@ from helpers import extract_file_ext, fetch_image
 
 def fetch_hubble_picture_by_id(img_id, folder="images/hubble"):
     api_base_url = "http://hubblesite.org/api/v3/image/"
+    response = requests.get(f"{api_base_url}{img_id}")
+    response.raise_for_status()
     try:
-        response = requests.get(f"{api_base_url}{img_id}")
-        response.raise_for_status()
-        try:
-            picture = response.json()['image_files'][-2]['file_url']
-        except IndexError:
-            picture = response.json()['image_files'][-1]['file_url']
-        img_url = f"http:{picture}"
-        file_ext = extract_file_ext(img_url)[1]
-        filename = f"{img_id}{file_ext}"
-        fetch_image(img_url, filename, folder)
-        return filename
-
-    except HTTPError:
-        print(f"Error {response.status_code}")
-        return None
+        picture = response.json()['image_files'][-2]['file_url']
+    except IndexError:
+        picture = response.json()['image_files'][-1]['file_url']
+    img_url = f"http:{picture}"
+    file_ext = extract_file_ext(img_url)[1]
+    filename = f"{img_id}{file_ext}"
+    fetch_image(img_url, filename, folder)
+    return filename
 
 
 def fetch_hubble_pictures_by_category(collection_name="holiday_cards"):
     url_template = "http://hubblesite.org/api/v3/images"
     payload = {"collection_name": collection_name}
-    try:
-        response = requests.get(url_template, params=payload)
-        response.raise_for_status()
-        json_img_ids = response.json()
-        img_ids = [json_img_id['id'] for json_img_id in json_img_ids]
-        files = []
-        for img_id in img_ids:
-            logging.info(f'fetch_hubble_pictures_by_category(): Fetching image id {img_id}...')
+    response = requests.get(url_template, params=payload)
+    response.raise_for_status()
+    json_img_ids = response.json()
+    img_ids = [json_img_id['id'] for json_img_id in json_img_ids]
+    files = []
+    for img_id in img_ids:
+        logging.info(f'fetch_hubble_pictures_by_category(): Fetching image id {img_id}...')
+        try:
             files.append(fetch_hubble_picture_by_id(img_id))
-        return files
-    except HTTPError:
-        print(f"Error {response.status_code}")
-        return None
+        except HTTPError as error:
+            print(f"Error {error}")
+    return files
 
 
 def main():
@@ -63,7 +57,10 @@ def main():
     args = parser.parse_args()
     folder = "images/hubble"
     Path(folder).mkdir(parents=True, exist_ok=True)
-    fetch_hubble_pictures_by_category(args.collection_name)
+    try:
+        fetch_hubble_pictures_by_category(args.collection_name)
+    except HTTPError as error:
+        print(f"Error {error}")
 
 
 if __name__ == "__main__":
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,19 +8,19 @@ from helpers import fetch_image
 
 def fetch_spacex_last_launch():
     api_url = "https://api.spacexdata.com/v4/launches/latest"
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()
-        pictures = response.json()['links']['flickr']['original']
-        for picture_number, picture in enumerate(pictures, start=1):
-            file_name = f"spacex{picture_number}.jpg"
-            fetch_image(picture, file_name, 'images/spacex')
-    except HTTPError:
-        print(f"Error {response.status_code}")
+    response = requests.get(api_url)
+    response.raise_for_status()
+    pictures = response.json()['links']['flickr']['original']
+    for picture_number, picture in enumerate(pictures, start=1):
+        file_name = f"spacex{picture_number}.jpg"
+        fetch_image(picture, file_name, 'images/spacex')
 
 
 def main():
-    fetch_spacex_last_launch()
+    try:
+        fetch_spacex_last_launch()
+    except HTTPError as error:
+        print(f"Error {error}")
 
 
 if __name__ == "__main__":
